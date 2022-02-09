eaba21b27812b877fe8bf9bbe01d646e75d93242
coosoti <charlesosoti@gmail.com>
2022-02-09 17:55:10 +0300

    [First API] created an Express Server with routers endpoints and controllers

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,4 @@ const router = express.Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from 'express';
-const bodyParser= require('body-parser');
 import router from './routes/index';
 
 const app = express();
 const port = process.env.DB_PORT || 5000;
 
 app.use(router);
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
