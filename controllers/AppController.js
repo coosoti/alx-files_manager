671dc2ab45c6015ef126a8506fa10420f0d33937
coosoti <charlesosoti@gmail.com>
2022-02-09 17:47:01 +0300

    [First API] created an Express Server with routers endpoints and controllers

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,14 +5,14 @@ class AppController {
   static getStatus(req, res) {
     const redis = redisClient.isAlive();
     const db = dbClient.isAlive();
-    res.status(200).send({ redis: redis, db: db });
+    res.status(200).send({ redis, db });
   }
 
   static async getStats(req, res) {
     const users = await dbClient.nbUsers();
     const files = await dbClient.nbFiles();
 
-    res.status(200).send({ users: users, files: files });
+    res.status(200).send({ users, files });
   }
 }
 
