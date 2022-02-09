4d9e01be020fa21368610f7d207bbf847453db13
coosoti <charlesosoti@gmail.com>
2022-02-10 01:00:24 +0300

    [Authenticate a user] added ebdpoints that authenticate a user- token based and retrieve a user based on the token

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,7 @@
 import { createHash } from 'crypto';
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -31,6 +33,38 @@ class UsersController {
       id: newUser.insertedId,
       email,
     });
+  }
+
+  static async getMe(req, res) {
+    const authToken = req.header('X-Token') || null;
+    if (!authToken) {
+      res.status(401).send({
+        error: 'Unauthorized',
+      });
+      return;
+    }
+    const token = `auth_${authToken}`;
+    const user = await redisClient.get(token);
+    if (!user) {
+      res.status(401).send({
+        error: 'Unauthorized',
+      });
+      return;
+    }
+    const users = dbClient.client.db().collection('users');
+    const userData = await users.findOne({
+      _id: ObjectId(user),
+    });
+    if (userData) {
+      res.status(200).send({
+        id: user,
+        email: userData.email,
+      });
+    } else {
+      res.status(401).send({
+        error: 'Unauthorized',
+      });
+    }
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
-import express from 'express';
+import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
-const router = express.Router();
+const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
 router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
+
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
 
 module.exports = router;
